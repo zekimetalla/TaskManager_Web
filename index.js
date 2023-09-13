2f520d0291395fdf95737878fa19cc4011d6049a
zekimetalla <144137167+zekimetalla@users.noreply.github.com>
2023-09-13 19:30:04 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -49,3 +49,63 @@ function newElement() {
     }
   }
 }
+
+fetch('https://localhost:7220/api/Task', {
+    method: 'GET',
+    headers: {
+        'Content-Type': 'application/json',
+        
+    },
+})
+.then(response => response.json())
+.then(data => {
+    // Handle the response data here
+})
+.catch(error => {
+    // Handle errors here
+});
+
+// ---------------------------------
+// ---------------------------------
+const postData = {
+  key1: 'value1',
+  key2: 'value2'
+};
+
+
+function createTask() {
+  // Capture task input from the input field
+  const taskInput = document.getElementById("myInput").value;
+
+  // Check if the input is empty
+  if (taskInput.trim() === "") {
+      alert("Please enter a task.");
+      return;
+  }
+
+  const taskData = {
+      title: taskInput,
+      isComplete: false, 
+  };
+
+  
+  fetch('https://localhost:7220/api/Task', {
+      method: 'POST',
+      headers: {
+          'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(taskData),
+  })
+  .then(response => response.json())
+  .then(data => {
+      
+      console.log('New Task:', data);
+
+      
+      document.getElementById("myInput").value = "";
+  })
+  .catch(error => {
+      
+      console.error('Error:', error);
+  });
+}
